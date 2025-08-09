96b4a875d2195db7037b0ced0d9c7a32895f85fe
pame0520 <pamelaespinozapotoy@gmail.com>
2025-08-09 11:24:28 -0600

    creacion de clases

--- src/utilidades/HashUtil.java
+++ src/utilidades/HashUtil.java
@@ -2,14 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package app;
+package utilidades;
 
 /**
  *
- * @author Ben
+ * @author pame
  */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("CommitOne");
-    }
+public class HashUtil {
+    
 }
